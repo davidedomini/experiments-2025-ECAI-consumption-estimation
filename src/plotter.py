1a197ae4b4fd7fe15835161a59efe51fb8dfba75
Davide Domini <davide.domini@unibo.it>
2025-04-09 16:47:30 +0200

    feat: add plot joules

--- src/plotter.py
+++ src/plotter.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 
-def beutify_model_name(model):
+def extract_sparsity(model):
     if "sparsity-0.0" in model:
         model_type = "0.0"
     elif "sparsity-0.3" in model:
@@ -22,7 +22,6 @@ def beutify_model_name(model):
     return model_type
 
 def plot(data, metric):
-    # sns.set_style("whitegrid")
     plt.figure(figsize=(10, 6))
     ax = sns.barplot(data=data, x="Algorithm", y=metric, palette="viridis",width=1)
 
@@ -46,7 +45,6 @@ def plot(data, metric):
 
 if __name__ == '__main__':
 
-        
     data_output_directory = Path('charts')
     data_output_directory.mkdir(parents=True, exist_ok=True)
 
@@ -56,6 +54,9 @@ if __name__ == '__main__':
     matplotlib.rcParams.update({'ytick.labelsize': 35})
     plt.rcParams.update({"text.usetex": True})
     plt.rc('text.latex', preamble=r'\usepackage{amsmath,amssymb,amsfonts}')
+
+
+    ################# PLOT INFERENCE TIME AND ACCURACY #################
 
     csv_files = glob.glob("data/inference-time*.csv") 
 
@@ -76,10 +77,10 @@ if __name__ == '__main__':
     }
 
     for file in csv_files:
-        model_type = beutify_model_name(file)
+        sparsity = extract_sparsity(file)
         df = pd.read_csv(file)
-        times[model_type].append(df['Time'].mean())
-        accuracies[model_type].append(df['Accuracy'].mean())
+        times[sparsity].append(df['Time'].mean())
+        accuracies[sparsity].append(df['Accuracy'].mean())
 
 
     result_time = [{"Algorithm": algo, "Mean Time (s)": sum(times) / len(times)} for algo, times in times.items()]
@@ -90,3 +91,42 @@ if __name__ == '__main__':
     plot(df_time, "Mean Time (s)")
     plot(df_acc, "Mean Accuracy")
 
+
+    ################# PLOT ENERGY CONSUMPTION #################
+
+    consumptions = {
+        "0.0": [],
+        "0.3": [],
+        "0.5": [],
+        "0.7": [],
+        "0.9": []
+    }
+
+    csv_files = glob.glob("data/inference-energy*False.csv")
+
+    for file in csv_files:
+        sparsity = extract_sparsity(file)
+        df = pd.read_csv(file, sep=';')
+        consumptions[sparsity].append(df['package_0'].mean())
+
+    print(consumptions)
+    
+    experiments = list(consumptions.keys())
+    consumption_values = [v[0] for v in consumptions.values()]
+
+    consumptions = pd.DataFrame({
+        'Experiment': experiments,
+        'Consumption': consumption_values
+    })
+    initial_consumption = consumptions['Consumption'].iloc[0]
+    consumptions['Relative Consumption'] = consumptions['Consumption'] / initial_consumption
+
+    plt.figure(figsize=(10, 6))
+    ax = sns.barplot(x='Experiment', y='Relative Consumption', data=consumptions, palette='rocket')
+
+    plt.title("Relative Energy Consumption (w.r.t. $\psi = 0.0$)", fontsize=16)
+    plt.ylabel("Relative  consumption")
+    plt.xlabel("$\psi$")
+    plt.ylim(0, 1.1)
+    plt.tight_layout()
+    plt.savefig('charts/relative_engergy_consumption.pdf', dpi=300)
